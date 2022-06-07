e6e3beb02b070f548aca3696e90e6c6a9a7da57a
kkochie <kristinekochie@gmail.com>
2022-06-07 19:38:10 -0400

    commit tree 0c05f34321df451cbf46d089032543fd7dbbd99d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue <= 42) {
+        return 42 - someValue;
+    } else {
+        return someValue - 42;
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
